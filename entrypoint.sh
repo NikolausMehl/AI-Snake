62d971579c53bb0e11892df0a2657af83948c03c
Nikolaus Mehl <nikolaus@devman3.com>
2024-05-05 15:13:07 +0200

    Fixed source copy

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@
 git clone https://github.com/NikolausMehl/AI-Snake.git /tmp/AI-Snake
 
 # Copy the contents of the repository to the default NGINX directory
-cp -a /tmp/AI-Snake/. /usr/share/nginx/html/
+cp -a /tmp/AI-Snake/src/. /usr/share/nginx/html/
 
 # Cleanup temporary directory
 rm -rf /tmp/AI-Snake
